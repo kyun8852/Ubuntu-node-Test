83e93229a23071e684c03267dc899d7d20e23034
kyun8852 <kyun8852@gmail.com>
2022-06-17 14:36:35 +0900

    asd

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 var express = require("express");
-var mysql = require("mysql");
 var cors = require("cors");
 const upload = require("express-fileupload");
 require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
-// var db_info = {
-//   host: process.env.DB_HOST,
-//   port: "3306",
-//   user: process.env.DB_USER,
-//   database: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-// };
 
 app.use(express.json());
 app.use(upload());
@@ -22,21 +14,10 @@ app.use(
     credentials: true,
   })
 );
-
-// var connection = mysql.createConnection(db_info);
-// connection.connect();
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.post("/upload", (req, res) => {
-  if (req.body.data) {
-    console.log(req.body.data);
-  }
-  res.send("OK");
-});
-
-app.post("/data", (req, res) => {
+app.post("/", (req, res) => {
   if (req.files) {
     var file = req.files.file;
     var filename = file.name;
@@ -48,17 +29,13 @@ app.post("/data", (req, res) => {
       }
     });
   }
-  // if (req.body.data) {
-  //   connection.query(
-  //     `UPDATE testevent SET InputData = "${req.body.data}" `,
-  //     function (err, results, fields) {
-  //       // testQuery 실행
-  //       if (err) {
-  //         console.log(err);
-  //       }
-  //     }
-  //   );
-  // }
+});
+
+app.post("/data", (req, res) => {
+  if (req.body.data) {
+    console.log(req.body.data);
+  }
+  res.send("OK");
 });
 
 app.get("/hello", (req, res) => {
